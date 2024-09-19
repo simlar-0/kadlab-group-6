9365c6da3d42993d2a7a8e3d370aff9815af158a
Simon Larsson <simlar-0@student.ltu.se>
2024-09-19 14:25:09 +0200

    Added variable nr of nodes to deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Default value
+DEFAULT_VALUE=50
+# Use the first argument if provided, otherwise use the default value
+VALUE=${1:-$DEFAULT_VALUE}
+
 # Step 1: Stop and remove all running Docker containers
 docker-compose down
 docker rm -f $(docker ps -aq)
@@ -8,4 +13,4 @@ docker rm -f $(docker ps -aq)
 docker build . -t kadlab
 
 # Step 3: Deploy the Docker Compose stack with scaling
-docker-compose up --scale kademliaNodes=50 -d
+docker-compose up --scale kademliaNodes=$VALUE -d
